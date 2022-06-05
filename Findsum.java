fb7a22c582700964de04e20c6bcd399bc40feb29
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 20:03:57 +0530

    Add files via upload

---
+++ Findsum.java
@@ -0,0 +1,19 @@
+//Find the sum of 1 to 10 natura
+public class Findsum {
+
+	public static void main(String[] args) {
+		int i,sum=0;
+		
+		i=1;
+		while(i<=10) {        //Condition
+			                //System.out.println("sum="+sum+"+"+i);
+			sum=sum+i;      //sum=0+1=1 sum=1+2=3 sum3+3=6
+			i=i+1;
+			
+			
+		}
+		
+         System.out.println("sum="+sum);
+	}
+
+}

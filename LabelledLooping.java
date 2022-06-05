ce8abc089855320125ad0050bbbc287729325345
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 20:04:13 +0530

    Add files via upload

---
+++ LabelledLooping.java
@@ -0,0 +1,19 @@
+
+public class LabelledLooping {
+
+	public static void main(String[] args) {
+	    
+	    	for(int i=1;i<=5;i++) {
+	    		
+	    		for(int j=1;i<=5;j++) {
+	    			if(i==2) {
+	    				break;
+	    			}
+	    			System.out.print("i="+i+"j="+j);
+	    		}
+	    		System.out.println();
+	    	}
+
+	}
+
+}

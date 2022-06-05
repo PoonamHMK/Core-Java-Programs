195b2d90c1cdafe4410db5516a3055615c7aea8a
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 18:47:01 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,14 @@
+package pack.com;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		int l,b,ar;
+		l=98;
+		b=76;
+		ar=l*b;
+		System.out.println("The area of rectangle of length "+l+" and breadth "+b+" is "+ar);
+
+	}
+
+}

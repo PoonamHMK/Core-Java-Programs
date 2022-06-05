f5c549eddd0dcbd6f602ed3b41554dde4e49f363
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 18:48:51 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,14 @@
+package pack.com;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		int b,h,a;
+		b=5;
+		h=10;
+		a=(b*h)/2;
+		System.out.println(" The area of triangle is "+a);
+
+	}
+
+}

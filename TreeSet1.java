4dec8c50fcbafa1a7756805a9312e9d9e940aa98
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 19:20:23 +0530

    Add files via upload

---
+++ TreeSet1.java
@@ -0,0 +1,19 @@
+package pack.poonam2;
+
+import java.util.HashSet;
+//import java.util.TreeSet;
+
+public class TreeSet1 {
+
+	public static void main(String[] args) {
+		HashSet<Integer>tob=new HashSet<Integer>();
+		
+		tob.add(76);
+		tob.add(10);
+		tob.add(58);
+		tob.add(-8);
+		System.out.println(tob);
+
+	}
+
+}

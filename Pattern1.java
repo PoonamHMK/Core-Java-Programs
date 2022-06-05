0dbd0f48affa412b66d5f6f061882670555f21ef
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 20:25:35 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,14 @@
+package ptrn;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+       String s="edubridge";
+       for(int i=0; i<s.length(); i++)
+       {
+    	   System.out.println(s.substring(0,i+1));
+       }
+
+	}
+
+}
---
+++ Pattern2.java
@@ -0,0 +1,13 @@
+package ptrn;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		  String s="HELLO";
+	       for(int i=4; i<s.length(); i--)
+	       {
+	    	   System.out.println(s.substring(0,i+1));
+	       }
+	}
+
+}

e1ec7976a25768f089fcd09e6ea3bbbd6c8170ce
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 20:18:25 +0530

    Add files via upload

---
+++ MyTreeMapClass.java
@@ -0,0 +1,18 @@
+package pack1;
+
+import java.util.TreeMap;
+
+public class MyTreeMapClass {
+
+	public static void main(String[] args) {
+		TreeMap<Integer,String> hp=new TreeMap<Integer,String>();
+		hp.put(20, "Sanika");
+		hp.put(10, "Hanika");
+		hp.put(30, "Poonam");
+		hp.put(40, "Radha");
+		hp.put(50, "Antara");
+    System.out.println(hp);
+
+	}
+
+}

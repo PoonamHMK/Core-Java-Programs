d650900cc901e20ddef2a8964bfc45e7beea052f
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 18:48:24 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,15 @@
+package pack.com;
+
+public class Square {
+
+	public static void main(String[] args) {
+		int s,a;
+		s=5;
+		a=s*s;
+		System.out.println(" The area of square is "+a);
+		
+		
+
+	}
+
+}

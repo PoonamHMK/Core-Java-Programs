6b87fa2d7ebe9ef2c8912407f1c9df3fe75f159b
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 19:17:58 +0530

    Add files via upload

---
+++ MyArrayList5.java
@@ -0,0 +1,14 @@
+package pack.poonam2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MyArrayList5 {
+
+	public static void main(String[] args) {
+		ArrayList<String> lst=new ArrayList<>();
+		
+
+	}
+
+}

624f222c00a15dcfbce15e7c0b8edd1b4e8ee4e7
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 18:12:31 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,15 @@
+package pack.com;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		int r,area;
+		double pi=3.142,a;
+		r=5;
+		a=pi*r*r;
+		System.out.println(" Area of circle is "+a);
+		
+
+	}
+
+}

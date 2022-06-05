74b305c4435a6f6d86b904f220f7dd5df9f3a5e9
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 20:17:31 +0530

    Add files via upload

---
+++ MyHashTableClass.java
@@ -0,0 +1,18 @@
+package pack1;
+
+import java.util.Hashtable;
+
+public class MyHashTableClass {
+
+		public static void main(String[] args) {
+			Hashtable<Integer,String> hp=new Hashtable<Integer,String>();
+			hp.put(20, "Sanika");
+			hp.put(10, "Hanika");
+			hp.put(30, "Poonam");
+			hp.put(5, "Radha");
+			hp.put(50, "Antara");
+	    System.out.println(hp);
+
+	}
+
+}

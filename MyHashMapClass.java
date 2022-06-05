f496b6ca7bd930aaa006e4cdc34add29a85d6989
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 20:17:08 +0530

    Add files via upload

---
+++ MyHashMapClass.java
@@ -0,0 +1,17 @@
+package pack1;
+
+import java.util.HashMap;
+
+public class MyHashMapClass {
+
+	public static void main(String[] args) {
+		HashMap<Integer,String> hp=new HashMap<Integer,String>();
+		hp.put(20, "Sanika");
+		hp.put(10, "Hanika");
+		hp.put(30, "Poonam");
+		hp.put(40, "Radha");
+		hp.put(50, "Antara");
+    System.out.println(hp);
+	}
+
+}

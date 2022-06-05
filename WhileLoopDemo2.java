5192ffacbe5b0363924a5900a17910854241b5e8
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 20:05:51 +0530

    Add files via upload

---
+++ WhileLoopDemo2.java
@@ -0,0 +1,15 @@
+//pgm to print num 100 to 1
+public class WhileLoopDemo2 {
+
+	public static void main(String[] args) {
+		int i;
+		i=100;
+		while(i>=1) {
+			System.out.println(i);
+			i=i-1;
+		}
+		
+
+	}
+
+}

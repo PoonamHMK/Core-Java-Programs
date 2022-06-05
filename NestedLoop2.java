9a7c511a202d2403a135bb4f43e0bebd3e575b80
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 20:05:08 +0530

    Add files via upload

---
+++ NestedLoop2.java
@@ -0,0 +1,15 @@
+
+public class NestedLoop2 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++) {     //rows
+			for(int j=1;j<=4;j++) {   //columns
+				System.out.print("@ ");
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}

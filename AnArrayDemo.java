a00d3309354f9ff2ea829616a27c864ad1005eb2
Poonam Muttagikar <100015630+PoonamHMK@users.noreply.github.com>
2022-06-05 18:54:01 +0530

    Add files via upload

---
+++ AnArrayDemo.java
@@ -0,0 +1,14 @@
+package Array.pack;
+
+public class AnArrayDemo {
+
+	public static void main(String[] args) {
+		int a[]= {9,10,5,45,12};   //declaration and initialization of an array
+		 
+		System.out.println("First  element = "+a[0]);
+		System.out.println("Second element = "+a[1]);
+		System.out.println("Third  element = "+a[2]);
+        System.out.println("Fourth element = "+a[3]);
+        System.out.println("Fifth  element = "+a[4]);
+}
+}
